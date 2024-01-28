91e05c5c6d61186de7fa51099a5c8453cd67f355
ashi <40967034+frqstbite@users.noreply.github.com>
2024-01-28 15:59:01 -0500

    Fixed stupid chest item

--- src/main/java/com/housesoup/stupid_chests/ModItems.java
+++ src/main/java/com/housesoup/stupid_chests/ModItems.java
@@ -4,22 +4,24 @@ import com.google.common.base.Supplier;
 import com.housesoup.stupid_chests.common.item.StupidChestItem;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    private static final DeferredRegister<Item> BLOCKS = DeferredRegister
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister
         .create(ForgeRegistries.ITEMS, StupidChestsMod.MODID);
     
     public static final RegistryObject<Item> STUPID_CHEST = register("stupid_chest", StupidChestItem::new);
+    public static final RegistryObject<Item> STUPID_KEY = register("stupid_key", () -> new Item(new Item.Properties().rarity(Rarity.RARE)));
 
     private static RegistryObject<Item> register(String name, Supplier<Item> sup) {
-        return BLOCKS.register(name, sup);
+        return ITEMS.register(name, sup);
     }
 
     public static void register(IEventBus modEventBus) {
-        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
     }
 }
